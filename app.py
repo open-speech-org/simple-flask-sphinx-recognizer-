b907d1c39c121749fe3b483123061895e7ae0afc
ma0c <ma0@contraslash.com>
2020-01-24 21:49:29 -0500

    modified service to work as expected

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-
 from flask import Flask
-from flask.wrappers import Response
 from flask.globals import request
 
 
@@ -13,44 +11,42 @@ recognizer = sr.Recognizer()
 
 @app.route("/", methods=["GET"])
 def index():
-    return Response(
-        {
-            "response": "Make a post to `/recognize` with a attached wav audio, sampled at 16000hz with name audio"
-        },
-        content_type="application/json",
-        status=200
-    )
+    return {
+        "response": "Make a post to `/recognize` with a attached wav audio, sampled at 16000hz with name audio"
+    }
 
 
 @app.route("/recognize", methods=["GET", "POST"])
 def recognize():
     if request.method == "POST":
         if "audio" not in request.files:
-            return Response(
-                {"response": "You must specify an audio parameter with a wav file"},
-                status=400
+            return (
+                {
+                    "response": "You must specify an audio parameter with a wav file"
+                },
+                400
             )
-        audio = sr.AudioData(request.files["audio"].read(), 16000, 1)
+        audio_file = sr.AudioFile(request.files["audio"])
+        with audio_file as source:
+            audio = recognizer.record(source)
+            hypothesis = recognizer.recognize_sphinx(
+                audio,
+                (
+                    'isolated_words_spa/words1.cd_semi_200',
+                    'isolated_words_spa/words1.lm.DMP',
+                    'isolated_words_spa/words1.dic'
+                )
+            )
+            print(hypothesis)
+        return {
+            "reponse": "Audio processed",
+            "hypothesis": hypothesis
+        }
 
-        hypothesis = recognizer.recognize_sphinx(
-            audio,
-            (
-                'isolated_words_spa/words1.cd_semi_200',
-                'isolated_words_spa/words1.lm.DMP',
-                'isolated_words_spa/words1.dic'
-            )
-        )
-        return Response(
-            {
-                "reponse": "Audio processed",
-                "hypothesis": hypothesis
-            }
-        )
     else:
-        return Response(
-            {"response": "Please use the POST method and specify audio parameter with a wav file"},
-            status=200
-        )
+        return {
+            "response": "Please use the POST method and specify audio parameter with a wav file"
+        }
 
 
 if __name__ == "__main__":
